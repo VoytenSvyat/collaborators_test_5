0d9a1aad4925d0a3975392186daf9d3231d05b52
StanislavVidulin <stas_vidulin@icloud.com>
2025-03-08 12:17:46 +0100

    added feature/remove-duplicates

--- vera.js
+++
@@ -1,8 +0,0 @@
-function groupByParity(arr) {
-    return {
-      even: arr.filter(num => num % 2 === 0),
-      odd: arr.filter(num => num % 2 !== 0)
-    };
-  }
-  
-  console.log(groupByParity([1, 2, 3, 4, 5, 6])); 
